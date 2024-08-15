c62ae1a3a411b4ca9c2e604d3971a1c18ff6b3f3
akara44 <ahmetkara2044@gmail.com>
2024-08-15 16:14:53 +0300

    Alt Kategoriler Frontend

--- app/Models/Urunler.php
+++ app/Models/Urunler.php
@@ -14,4 +14,9 @@ class Urunler extends Model
         
         return $this->belongsTo(Altkategoriler::class , 'altkategori_id', 'id');
     }
+
+    public function kategoriler(){
+        
+        return $this->belongsTo(Kategoriler::class , 'kategori_id', 'id');
+    }
 }
