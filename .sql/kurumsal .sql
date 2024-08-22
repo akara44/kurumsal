7169fea7bf68bc999debae2af07454fd269405f3
akara44 <ahmetkara2044@gmail.com>
2024-08-22 11:29:24 +0300

    Database Updated

--- .sql/kurumsal .sql
+++ .sql/kurumsal .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 08 Ağu 2024, 10:08:31
+-- Üretim Zamanı: 22 Ağu 2024, 10:28:30
 -- Sunucu sürümü: 10.4.32-MariaDB
 -- PHP Sürümü: 8.2.12
 
@@ -49,7 +49,8 @@ INSERT INTO `alt_kategorilers` (`id`, `kategori_id`, `altkategori_adi`, `altkate
 (24, 31, 'Top', 'top', 'Futbol , Basketbol , Buz Hokeyi , Judo', 'Oyun', 'upload/altkategoriler/1806283415203690.jpeg', '2024-08-02 10:41:41', NULL),
 (25, 31, 'Logo', 'logo', 'Grafik Tasarım , Logo', 'Kurumsal', 'upload/altkategoriler/1806710701220353.jpeg', '2024-08-07 03:53:13', NULL),
 (26, 30, 'Ulaşım', 'ulasim', 'Uçak , tren , karayolu', 'Hizmet', 'upload/altkategoriler/1806710793569658.jpg', '2024-08-07 03:54:41', NULL),
-(37, 29, 'Portfolyo', 'portfolyo', 'Ulaşım , logo', 'Kurumsal', 'upload/altkategoriler/1806710904816297.jpeg', '2024-08-07 03:56:27', NULL);
+(37, 29, 'Portfolyo', 'portfolyo', 'Ulaşım , logo', 'Kurumsal', 'upload/altkategoriler/1806710904816297.jpeg', '2024-08-07 03:56:27', NULL),
+(39, 29, 'Yapı', 'yapi', 'Uçak , tren , karayolu,Köprü', 'Yapılar', 'upload/altkategoriler/1808003434178753.jpeg', '2024-08-21 10:20:39', NULL);
 
 -- --------------------------------------------------------
 
@@ -73,7 +74,7 @@ CREATE TABLE `banners` (
 --
 
 INSERT INTO `banners` (`id`, `baslik`, `alt_baslik`, `url`, `video_url`, `resim`, `created_at`, `updated_at`) VALUES
-(1, 'Size en iyi ürünü en kısa sürede vereceğim.', 'Temiz ve kullanıcı dostu deneyimler oluşturmaya odaklanmış Akara44 merkezli bir ürün tasarımı ve web tasarımcıyım', 'http://127.0.0.1:8000/', 'http://127.0.0.1:8000/dashboard', 'upload/banner/1806729014349017.jpeg', NULL, '2024-08-07 08:44:18');
+(1, 'Size en iyi ürünü en kısa sürede vereceğim.', 'Temiz ve kullanıcı dostu deneyimler oluşturmaya odaklanmış Akara44 merkezli bir ürün tasarımı ve web tasarımcıyım.', 'http://127.0.0.1:8000/', 'http://127.0.0.1:8000/dashboard', 'upload/banner/1807444613276527.png', NULL, '2024-08-15 06:18:26');
 
 -- --------------------------------------------------------
 
@@ -113,9 +114,11 @@ CREATE TABLE `kategorilers` (
 --
 
 INSERT INTO `kategorilers` (`id`, `kategori_adi`, `kategori_url`, `anahtar`, `aciklama`, `resim`, `created_at`, `updated_at`) VALUES
-(29, 'Çalışmalarımız', 'calismalarimiz', 'Çalışmalarımız', 'Çalışmalarımız', 'upload/kategoriler/1806279781857533.jpeg', '2024-08-02 09:43:56', NULL),
-(30, 'Çözümlerimiz', 'cozumlerimiz', 'Çözümlerimiz', 'Çözümlerimiz', 'upload/kategoriler/1806279811651140.jpeg', '2024-08-02 09:44:24', NULL),
-(31, 'Ürünlerimiz', 'urunlerimiz', 'Ürünlerimiz', 'Ürünlerimiz', 'upload/kategoriler/1806279838601974.jpeg', '2024-08-02 09:44:50', NULL);
+(29, 'Çalışmalarımız', 'calismalarimiz', 'Çalışmalarımız', 'Size En iyi deneyimlerinizi sunmak için durmadan gelişiyoruz.', 'upload/kategoriler/1806279781857533.jpeg', '2024-08-02 09:43:56', '2024-08-15 07:07:18'),
+(30, 'Çözümlerimiz', 'cozumlerimiz', 'Çözümlerimiz', 'Size En iyi deneyimlerinizi sunmak için durmadan gelişiyoruz.', 'upload/kategoriler/1806279811651140.jpeg', '2024-08-02 09:44:24', '2024-08-15 07:07:13'),
+(31, 'Ürünlerimiz', 'urunlerimiz', 'Ürünlerimiz', 'Size En iyi deneyimlerinizi sunmak için durmadan gelişiyoruz.', 'upload/kategoriler/1806279838601974.jpeg', '2024-08-02 09:44:50', '2024-08-15 07:07:08'),
+(35, 'Teknolojilerimiz', 'teknolojilerimiz', 'Teknoloji', 'Size En iyi deneyimlerinizi sunmak için durmadan gelişiyoruz.', 'upload/kategoriler/1806895810613089.jpeg', '2024-08-09 04:55:27', '2024-08-15 07:07:02'),
+(36, 'Hizmetlerimiz', 'hizmetlerimiz', 'Hizmetlerimiz', 'Size En iyi deneyimlerinizi sunmak için durmadan gelişiyoruz.', 'upload/kategoriler/1806895876562787.jpeg', '2024-08-09 04:56:30', '2024-08-15 07:06:13');
 
 -- --------------------------------------------------------
 
@@ -141,7 +144,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (5, '2024_07_24_115725_create_banners_table', 2),
 (6, '2024_07_31_093650_create_kategorilers_table', 3),
 (7, '2024_08_01_104036_create_alt_kategorilers_table', 4),
-(8, '2024_08_07_080002_create_urunlers_table', 5);
+(8, '2024_08_07_080002_create_urunlers_table', 5),
+(9, '2024_08_22_074127_create_blogkategorilers_table', 6);
 
 -- --------------------------------------------------------
 
@@ -189,7 +193,7 @@ CREATE TABLE `urunlers` (
   `tag` varchar(255) DEFAULT NULL,
   `anahtar` varchar(255) DEFAULT NULL,
   `aciklama` varchar(255) DEFAULT NULL,
-  `metin` varchar(255) DEFAULT NULL,
+  `metin` text DEFAULT NULL,
   `resim` varchar(255) DEFAULT NULL,
   `durum` tinyint(1) NOT NULL DEFAULT 0,
   `sirano` int(11) NOT NULL DEFAULT 1,
@@ -202,7 +206,10 @@ CREATE TABLE `urunlers` (
 --
 
 INSERT INTO `urunlers` (`id`, `kategori_id`, `altkategori_id`, `baslik`, `url`, `tag`, `anahtar`, `aciklama`, `metin`, `resim`, `durum`, `sirano`, `created_at`, `updated_at`) VALUES
-(1, 0, 0, 'başlık', 'başlık', 'tag', NULL, NULL, 'metin', NULL, 1, 1, NULL, '2024-08-07 10:00:07');
+(13, 30, 23, 'deneme', 'deneme', 'Editör,Laravel', 'deneme', 'deneme', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non quam ullamcorper, tempor massa quis, egestas velit. In hac habitasse platea dictumst. Nullam sed metus pharetra, euismod risus nec, pellentesque ligula. Nam sit amet purus feugiat, feugiat enim id, pulvinar nibh. In lacinia accumsan ante et ullamcorper. Aenean et molestie ligula. Pellentesque fermentum sapien sed purus bibendum, at rutrum turpis feugiat. Phasellus tristique magna vitae quam cursus dictum. Vestibulum pharetra condimentum pulvinar. Ut eleifend ligula ut dui interdum auctor. Sed porta, felis eu mollis rhoncus, libero metus faucibus tortor, at rhoncus nisl eros ac sem. Integer ut lacinia est.</p>\r\n<p>Aliquam id laoreet ante. Cras accumsan massa sed augue sodales, vel blandit leo viverra. Pellentesque ornare mauris eu eros iaculis dignissim id eget nulla. Vivamus dignissim placerat odio, id placerat ante sagittis ac. Donec pharetra, quam eu semper convallis, velit diam mattis quam, sit amet hendrerit diam leo quis risus. Nam fermentum venenatis justo nec porttitor. Donec eu libero massa.</p>\r\n<p>Maecenas ut facilisis ex. In hac habitasse platea dictumst. Curabitur a blandit orci, et elementum tortor. Mauris maximus massa massa. Aliquam malesuada scelerisque fringilla. Sed ut mattis quam. Aliquam vehicula ipsum accumsan arcu tincidunt eleifend. Nulla vitae nunc vulputate, accumsan eros quis, dapibus turpis. Donec quis varius mauris. Suspendisse tincidunt, leo sed ultrices sagittis, erat arcu pharetra diam, nec vestibulum turpis nibh a turpis.</p>', 'upload/urunler/1807978007487493.jpeg', 1, 4, '2024-08-21 03:36:30', '2024-08-21 06:30:36'),
+(14, 30, 26, 'Kurumsal Log', 'kurumsal-log', 'Adobe ,luustrator', 'Kurumsal LoGO', 'logo', '<p>Deneme</p>', 'upload/urunler/1807978678067033.png', 1, 8, '2024-08-21 03:47:10', '2024-08-21 09:45:30'),
+(15, 30, 26, 'Taşımacılık', 'tasimacilik', 'Editör,Laravel,Taşıyoruz', 'Uçak , tren , karayolu', 'Taşımacılık Nedir?', '<p style=\"font-weight: 400;\"><strong>Taşımacılık Nedir?</strong></p>\r\n<p style=\"font-weight: 400;\">Herhangi bir malın ya da y&uuml;k&uuml;n bir yerden ama&ccedil;lanan başka bir yere nakliye edilmesi durumuna taşıma adı verilmektedir. Taşımacılık işinde &ouml;nemli olan nokta, y&uuml;k&uuml;n taşınacağı yere hi&ccedil;bir zarar almadan ve hızlı bir şekilde ulaştırılmasıdır. Uluslararası taşımacılık nedir? Şeklindeki bir soruya da cevap olarak ; iki &uuml;lke arasındaki herhangi bir taşıma işi olarak tanımlanmaktadır. Bu taşıma işi deniz yolu, hava yolu ve kara yolu olarak ger&ccedil;ekleştirilmektedir.</p>\r\n<p style=\"font-weight: 400;\">Bir &uuml;lkeden başka bir &uuml;lkeye yapılması planlanan nakliye işinden malların minimum derecede dikkatli bir şekilde ve titizlik ile muhafaza edilmesi gerekmektedir. G&uuml;nlerce s&uuml;ren hatta ve hatta belki de haftalar s&uuml;ren bu zorlu seyahatte eşyaların hasar g&ouml;rme olasılığı son derece y&uuml;ksek bir ihtimaldir. B&uuml;t&uuml;n bunlar değerlendirildiği zaman ise kurumsal ve bu işi profesyonel bir şekilde ger&ccedil;ekleştiren firmaların tercih edilmesi &ccedil;ok daha uygun olacaktır.</p>', 'upload/urunler/1808001013577365.jpg', 1, 5, '2024-08-21 09:42:11', '2024-08-22 03:49:45'),
+(17, 29, 39, 'Yavuz Sultan Selim Köprüsü', 'yavuz-sultan-selim-koprusu', 'Köprü,Ulaşım,Kamu,HGS', 'Uçak , tren , karayolu,Zincirleme', 'Kamu', '<p>T&uuml;rkiye\'deki 3. k&ouml;pr&uuml;, resm&icirc; adıyla <strong>Yavuz Sultan Selim K&ouml;pr&uuml;s&uuml;</strong> olarak bilinir. İstanbul\'da, Avrupa ve Asya kıtalarını birleştiren &uuml;&ccedil;&uuml;nc&uuml; boğaz k&ouml;pr&uuml;s&uuml;d&uuml;r.</p>', 'upload/urunler/1808003525667381.jpeg', 1, 3, '2024-08-21 10:22:06', NULL);
 
 -- --------------------------------------------------------
 
@@ -227,8 +234,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `resim`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(5, 'Deneme2', 'deneme2@gmail.com', '202408071148laravel.png', '2024-07-20 12:06:54', '$2y$10$x7nh6kaFZBdustwbzU5kxuabC7wglgb8f/7WhlWL/x7gCuJ2zOmve', '2qw9lOQRMsL0TMIjv0QraPYt0t49ScQfJtyXDwKYbJB10NllByF2VEMz6tqS', '2024-07-20 12:06:08', '2024-08-07 08:48:31'),
-(6, 'Ahmet Kara', 'ahmetkara2044@gmail.com', '202407240943Adsız.png', '2024-07-24 06:37:14', '$2y$10$.QHfNFsfV8y4K6OPcdyYre7yhHhCl45C01pHi8tykwyt697eJz5Ga', NULL, '2024-07-24 06:08:14', '2024-07-31 03:19:05'),
+(6, 'Ahmet Kara', 'ahmetkara2044@gmail.com', '202407240943Adsız.png', '2024-07-24 06:37:14', '$2y$10$ThVbC/Ag4bFjDmQGYd7a2e9HRwMGuJTWcBx0VJZXh7k.HsI/9sgVS', '2NY4sd3TAlGAhLAAPph5mgMpktwZksGFpxf7Y6fmc6EXFeB6FZdjaMgGdNnL', '2024-07-24 06:08:14', '2024-08-08 07:05:15'),
 (7, 'deneme22', 'deneme22@gmail.com', NULL, NULL, '$2y$10$ppXvOSxCIXo/9afBr2HXU.AfRaOGGO9JrcD1S8Q2GabrCAIzBwt5i', NULL, '2024-07-24 06:30:35', '2024-07-24 06:30:35');
 
 --
@@ -301,7 +307,7 @@ ALTER TABLE `users`
 -- Tablo için AUTO_INCREMENT değeri `alt_kategorilers`
 --
 ALTER TABLE `alt_kategorilers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `banners`
@@ -319,13 +325,13 @@ ALTER TABLE `failed_jobs`
 -- Tablo için AUTO_INCREMENT değeri `kategorilers`
 --
 ALTER TABLE `kategorilers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `personal_access_tokens`
@@ -337,7 +343,7 @@ ALTER TABLE `personal_access_tokens`
 -- Tablo için AUTO_INCREMENT değeri `urunlers`
 --
 ALTER TABLE `urunlers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `users`
