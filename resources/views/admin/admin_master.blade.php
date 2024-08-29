d6d3a61e345c2eed04b8df7bc92d1b863d5d5ee2
akara44 <ahmetkara2044@gmail.com>
2024-08-29 11:06:48 +0300

    Listing of Blog Content

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -256,6 +256,7 @@
         handleCheckboxChange('.icerikler', '/blog/kategori/durum');
         handleCheckboxChange('.altkategoriler', '/altkategoriler/durum');
         handleCheckboxChange('.kategoriler', '/kategori/durum');
+        // handleCheckboxChange('.metinler', '/kategori/durum');
     });
 </script>
     </body>
